9c17b770f98ca3748889cfa365833d7b95e3c302
LeNguyenQuynhPhuong <phuong_dpm245478@student.agu.edu.vn>
2025-10-04 21:09:49 +0700

    hehe

---
+++ Chuong2_Cau7.py
@@ -0,0 +1,7 @@
+"""Câu 7: Trình bày một số cách nhập dữ liệu từ bàn phím."""
+# python sẽ cung cấp hàm input() để nhập mộy dãy kí tự từ bàn phím 
+# ví dụ cho này thì ta có :
+a = int (input('Nhập số a:'))
+b = int (input('Nhập số b:'))
+print('Tổng a + b =', a + b)
+# chương trình trên chạy thì ta nhập giá trị a và b từ bàn phím
---
+++ Chuong2_Cau8.py
@@ -0,0 +1,11 @@
+'''Câu 8: Trình bày các loại lỗi khi lập trình và cách bắt lỗi trong Python'''
+# Các loại lỗi khi lập trình:
+# 1. Lỗi cú pháp (Syntax Error): Xảy ra khi mã nguồn không tuân thủ các quy tắc cú pháp của ngôn ngữ lập trình.
+# 2. Lỗi thời gian chạy (Runtime Error): Xảy ra khi chương trình đang chạy và gặp phải một tình huống không thể xử lý được, như chia cho số 0.
+# 3. Lỗi logic (Logical Error): Xảy ra khi chương trình chạy mà không tạo ra kết quả đúng như mong đợi do lỗi trong logic của mã nguồn.
+# 4. Lỗi ngoại lệ (Exception): Xảy ra khi có sự cố không mong muốn trong quá trình thực thi chương trình, như truy cập vào một phần tử không tồn tại trong danh sách.
+# Cách bắt lỗi trong Python:
+"""try:
+    # Đoạn mã có thể gây lỗi
+except Tên_Lỗi:
+   """
